ca697fa9500b70129a70ad58fc2dd41414583643
Mary Dickson Diaz <mdickson@gmail.com>
2016-12-04 10:22:04 -0800

    change to components

---
+++ app/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class HomePage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to Code 501</h1>
+        <h2>such web, very components, wow</h2>
+      </div>
+    )
+  }
+}
+
+export default HomePage
--- app/index.js
+++ app/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import HomePage from './components/HomePage/HomePage'
+
 if (process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf')
 }
 
-const page = (
-  <div>
-    <h1>Welcome to Code 501</h1>
-    <h2>Building Web Apps with React + Redux</h2>
-  </div>
-)
+// this is a jsx element
+// js-standard is the linter of choice
 
-render(page, document.getElementById('app'))
+// const page = (
+//   <div>
+//     <h1>Welcome to Code 501</h1>
+//     <h2>Building Web Apps with React + Redux</h2>
+//   </div>
+// )
+
+// render function can only take 1 block, 1 thing. so the page element MUST be wraped in a div
+
+render(<HomePage />, document.getElementById('app'))
