34882af50f48d4c0aef2696c1ae98cefa0e8391b
Mary Dickson Diaz <mdickson@gmail.com>
2016-12-04 12:01:13 -0800

    change to const

--- app/components/Bio/Bio.js
+++ app/components/Bio/Bio.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import _ from 'lodash'
 
-class Bio extends React.Component {
-  render () {
-    return (
-      <div className='Bio'>
-        {/* <img src={this.props.imdSrc} alt={this.props.imgAlt} /> */}
-        <p>{this.props.name} is {this.props.age}.</p>
-        <p>{_.capitalize(this.props.pronoun.nominative)} enjoys {this.props.favoriteActivity}.</p>
-      </div>
-    )
-  }
+const Bio = ({ imgSrc, imgAlt, name, age, pronoun, favoriteActivity }) =>
+  <div className='Bio'>
+    {/* <img src={imgSrc} alt={imgAlt} /> */}
+    <p>{name} is {age}.</p>
+    <p>{_.capitalize(pronoun.nominative)} enjoys {favoriteActivity}.</p>
+  </div>
+
+Bio.propTypes = {
+  name: React.PropTypes.string,
+  age: React.PropTypes.number,
+  pronoun: React.PropTypes.object,
+  favoriteActivity: React.PropTypes.string
 }
 
 export default Bio
